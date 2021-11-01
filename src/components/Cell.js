b688af7fef726373a6a19491dd46c0d4ee8da3a8
Rachel Solov <rsolov23@yahoo.com>
2021-10-31 19:41:10 -0500

    added styledcell styled component

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,4 +1,10 @@
 import React from "react";
+import { StyledCell } from "./styles/StyledCell";
+import { TETROMINOS } from "../tetrominos";
 
-const Cell = ({ type }) => <div>cell</div>;
+const Cell = ({ type }) => (
+  <StyledCell type={"L"} color={TETROMINOS[type].color}>
+    cell
+  </StyledCell>
+);
 export default Cell;
